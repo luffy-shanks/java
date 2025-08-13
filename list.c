71ed3a38929eb823c73dd6b9bd6facb31e26ad25
luffy-shanks <kudipudigowtham2@gmail.com>
2025-08-13 16:11:13 +0530

    Add files via upload

---
+++ list.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ int num;
+ struct node *nextptr;
+ }*
+ void createnodelist(int n);
+ void insert node at middlelist(int num,int pos);
+ void displaylist();
+  
+  int main()
+  {
+  int num,pos;
+  
